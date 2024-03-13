9c58d58403a3c0f1ea08e06cf45e8f7729fa2fe5
David-MarCle <martinezclementedavid@gmail.com>
2024-03-13 21:00:23 +0100

    Header

--- web-client/src/App.svelte
+++ web-client/src/App.svelte
@@ -1,29 +1,15 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+ 
+  import Header from './header/Header.svelte'
 </script>
 
 <main>
+  <Header/>
   <div>
-    <h1>prueba</h1>
+    
   </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+ 
 </style>
