60bf7ce38cf1b041ecd43952f8f2ee6c10985cea
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-15 12:46:04 +0100

    Secured password

--- web-client/src/App.svelte
+++ web-client/src/App.svelte
@@ -1,20 +1,22 @@
 <script>
   import Header from "./lib/header/Header.svelte";
   import Map from "./lib/Map.svelte";
+
   export let ready;
+  import {API_KEY} from "../password.js"
 </script>
 
 <svelte:head>
   <script
     defer
     async
-    src="https://maps.googleapis.com/maps/api/js?key=API&callback=initMap"
+    src={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`}
   ></script>
 </svelte:head>
 
 {#if ready}
   <Header />
-  <Map/>
+  <Map />
 {/if}
 
 <style>
